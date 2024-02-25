cd5156031c9ccfa5da2368e6d5bafb7f1bf40435
Shaun Kennedy <shaun@shaunpkennedy.com>
2024-02-25 13:25:25 -0600

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+Rails.application.routes.draw do
+  #get "about-us", to: "about#index", as: :about
+  get "about", to: "about#index"
+
+  root to: "main#index"
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+end
