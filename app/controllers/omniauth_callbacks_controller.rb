8f4c8f734ed5198d23f9350d4f04ce17a6edef2e
Shaun Kennedy <shaun@shaunpkennedy.com>
2024-03-03 14:13:58 -0600

    Part 25: Twitter Account Model

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,32 @@
 class OmniauthCallbacksController < ApplicationController
-
+    before_action :require_user_logged_in!
     def twitter
-            render plain: "Success"
-    end
+        if Current.user.twitter_accounts.any?
 
+            twitter_account = Current.user.twitter_accounts.where(username: auth.info.nickname).first_or_initialize
+
+            twitter_account.update(
+                name:auth.info.name,
+                username: auth.info.nickname,
+                image: auth.info.image,
+                token: auth.credentials.token,
+                secret: auth.credentials.secret,
+                )
+                redirect_to root_path, notice: "Successfully updated your account"
+                #redirect_to twitter_accounts_path, notice: "Successfully updated your account"
+        else # If there is no twitter account associated
+
+            TwitterAccount.create(user_id: Current.user[:id],username: auth.info.nickname, name:auth.info.name,
+                image: auth.info.image,
+                token: auth.credentials.token,
+                secret: auth.credentials.secret, ) 
+            
+                redirect_to root_path, notice: "Successfully connected your account"
+                #redirect_to twitter_accounts_path, notice: "Successfully connected your account"
+        end
+
+    end
+    def auth
+        request.env["omniauth.auth"]
+    end
 end
