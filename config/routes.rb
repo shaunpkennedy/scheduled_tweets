fc69be4803c3e22b3f72711638e80005d517d644
Shaun Kennedy <shaun@shaunpkennedy.com>
2024-03-03 12:54:11 -0600

    Part 24: OmniAuth 2.0 URLs

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
   get "password/reset/edit", to: "password_resets#edit"
   patch "password/reset/edit", to: "password_resets#update"
 
+  get "/auth/twitter/callback", to: "omniauth_callbacks#twitter"
+  
   root to: "main#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
