d398ecb3a931a7201c552b87007e02db6ac60aeb
Shaun Kennedy <shaun@shaunpkennedy.com>
2024-03-05 22:13:27 -0600

    Preparing for deploying to Render

--- app/jobs/tweet_job.rb
+++ app/jobs/tweet_job.rb
@@ -4,6 +4,7 @@ class TweetJob < ApplicationJob
   def perform(*args)
     return if tweet.published?
 
+    # Rescheduled a tweet to the future
     return if tweet.publish_at > Time.Current
 
     tweet.publish_to_twitter!
